e5d13ea740de849dedbd6b1e0aef797937af6bc1
Amy May Nguyen <anguye10@uchicago.edu>
2025-02-26 03:30:40 +0000

    Complete challenge, although not stretch goal

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,28 +22,37 @@ class User < ApplicationRecord
   ## Direct associations
 
   # User#comments: returns rows from the comments table associated to this user by the author_id column
+  has_many(:comments, class_name: "Comment", foreign_key: "author_id", primary_key: "id")
 
   # User#own_photos: returns rows from the photos table  associated to this user by the owner_id column
+  has_many(:own_photos, class_name: "Photo", foreign_key: "owner_id", primary_key: "id")
 
   # User#likes: returns rows from the likes table associated to this user by the fan_id column
+  has_many(:likes, class_name: "Like", foreign_key: "fan_id", primary_key: "id")
 
   # User#sent_follow_requests: returns rows from the follow requests table associated to this user by the sender_id column
+  has_many(:sent_follow_requests, class_name: "FollowRequest", foreign_key: "sender_id", primary_key: "id")
 
   # User#received_follow_requests: returns rows from the follow requests table associated to this user by the recipient_id column
+  has_many(:received_follow_requests, class_name: "FollowRequest", foreign_key: "recipient_id", primary_key: "id")
 
 
   ### Scoped direct associations
 
   # User#accepted_sent_follow_requests: returns rows from the follow requests table associated to this user by the sender_id column, where status is 'accepted'
+  # has_many(:accepted_sent_follow_requests, -> { accepted }, class_name: "FollowRequest", foreign_key: "sender_id")
 
   # User#accepted_received_follow_requests: returns rows from the follow requests table associated to this user by the recipient_id column, where status is 'accepted'
+  # has_many(:accepted_received_follow_requests, -> {accepted}, class_name: "FollowRequest")
 
 
   ## Indirect associations
 
   # User#liked_photos: returns rows from the photos table associated to this user through its likes
+  has_many(:liked_photos, through: :likes, source: :photo)
 
   # User#commented_photos: returns rows from the photos table associated to this user through its comments
+  has_many(:commented_photos, through: :comments, source: :photo)
 
 
   ### Indirect associations built on scoped associations
@@ -51,8 +60,8 @@ class User < ApplicationRecord
   # User#followers: returns rows from the users table associated to this user through its accepted_received_follow_requests (the follow requests' senders)
 
   # User#leaders: returns rows from the users table associated to this user through its accepted_sent_follow_requests (the follow requests' recipients)
-
   # User#feed: returns rows from the photos table associated to this user through its leaders (the leaders' own_photos)
 
   # User#discover: returns rows from the photos table associated to this user through its leaders (the leaders' liked_photos)
+
 end
